fb7657527154a8b365a030b2fb443681f194a58e
wu <1307944364@qq.com>
2022-11-07 14:19:33 +0800

    冒泡二分法查找

---
+++ 冒泡二分法/冒泡二分法/冒泡二分法查找.cpp
@@ -0,0 +1,38 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+int main()
+{
+	int a[10];
+	int i, j, t, n;
+	int low = 0, high = 9, mid, found=0;
+	for (i = 0; i < 10; i++)
+		scanf("%d", &a[i]);
+	for (j = 0; j < 9; j++) {
+		for (i = 0; i < 9 - j; i++)
+		{
+			if (a[i] > a[i + 1]) {
+				t = a[i];
+				a[i] = a[i + 1];
+				a[i + 1] = t;
+			}
+		}
+	}
+	printf("put in number with you want:");
+	scanf("%d", &n);
+	while (low <= high) {
+		mid = (low + high) / 2;
+		if (n == a[mid]) {
+			found = 1;
+			break;
+		}
+		else if (n < a[mid])
+			high = mid - 1;
+		else low = mid + 1;
+	}
+	if (found == 1) {
+		printf("your number is %d\n", mid);
+	}
+	else
+		printf("there is no %d",n);
+	return 0;
+}
